627558a78ff64bac93b55efa307abcb235e5bbf0
Jatin Garg <jgarg8440@gmail.com>
2023-10-29 00:00:57 +0530

    Second commit

--- public/app.js
+++ public/app.js
@@ -1,8 +1,12 @@
-const btn = document.querySelector(".open-close");
-btn.addEventListener("click", () => {
-    if (btn.innerHTML === "Open") {
-        btn.innerHTML = "Close";
-        btn.style.backgroundColor = "red";
-    } 
-});
+// Select all elements with the class "open-close"
+const openCloseButtons = document.querySelectorAll(".open-close");
 
+// Add a click event listener to each button
+openCloseButtons.forEach(btn => {
+    btn.addEventListener("click", () => {
+        if (btn.innerHTML === "Open") {
+            btn.innerHTML = "Close";
+            btn.style.backgroundColor = "red";
+        } 
+    });
+});
